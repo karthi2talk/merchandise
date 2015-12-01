e8612bf7250666baff6fbdedc3a8c6b5ef9f4691
karthi2talk <karthi2talk@gmail.com>
2015-12-01 10:48:27 +0530

    HY5.5CMRC Merchandise intial import :  Creating a code base for your project

---
+++ merchandise/merchandisecockpits/gensrc/de/hybris/merchandise/cockpits/constants/GeneratedMerchandiseCockpitsConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 28, 2015 4:28:57 PM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *  
+ */
+package de.hybris.merchandise.cockpits.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedMerchandiseCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "merchandisecockpits";
+	
+	protected GeneratedMerchandiseCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
