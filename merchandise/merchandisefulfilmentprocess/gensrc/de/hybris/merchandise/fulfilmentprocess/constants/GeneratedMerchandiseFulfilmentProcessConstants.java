d9eeef348b4e192966e289efd3f8fdcece5c74db
karthi2talk <karthi2talk@gmail.com>
2015-12-01 11:27:16 +0530

    HY5.5CMRC Merchandise intial import :  Creating a code base for your project

---
+++ merchandise/merchandisefulfilmentprocess/gensrc/de/hybris/merchandise/fulfilmentprocess/constants/GeneratedMerchandiseFulfilmentProcessConstants.java
@@ -0,0 +1,44 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 28, 2015 4:28:57 PM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *  
+ */
+package de.hybris.merchandise.fulfilmentprocess.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedMerchandiseFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "merchandisefulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedMerchandiseFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
